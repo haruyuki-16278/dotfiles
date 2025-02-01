29e6bd5033be1cb6959c22260dbe31d5e90b096c
haruyuki@hailfall.macbook <haruyuki@relicsnow.info>
2025-02-01 18:48:17 +0900

    初期設定用スクリプトを改良

--- init.sh
+++ init.sh
@@ -1,10 +1,24 @@
 #!/bin/sh
 
+# ディレクトリ名を取得
 dir="$(dirname "$0")"
 cd "$dir"
 
-echo "$PWD"
+# リンク作成先を確認
+echo "make links to $PWD . ok? (y/N)"
+read ok
+if [ "$ok" = "y" ]; then
+    # リンク作成
+    echo "link .zshrc"
+    ln -s $PWD/.zshrc ~/.zshrc
+    echo "link .vimrc"
+    ln -s $PWD/.vimrc ~/.vimrc
+    echo "link .tmux.conf"
+    ln -s $PWD/.tmux.conf ~/.tmux.conf
 
-ln -s $dir/.zshrc ~/.zshrc
-ln -s $dir/.vimrc ~/.vimrc
-ln -s $dir/.tmux.conf ~/.tmux.conf
+    echo "done!"
+else
+    # キャンセルされたらエラーコードありで終了
+    echo "cancel."
+    exit 1
+fi
